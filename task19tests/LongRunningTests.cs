010cc2288fe2628e35c40d9a89e08b082fbdf864
ErikVlad <erikvlad69@mail.ru>
2026-07-15 22:33:59 +0600

    feat: complete task19 long running commands with hard stop execution

---
+++ task19tests/LongRunningTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using Xunit;
+using task18;
+using task19;
+
+namespace task19tests;
+
+public class LongRunningTests
+{
+    [Fact]
+    public void TestCommand_ShouldRunFiveInstancesThreeTimes_ThenStop()
+    {
+        var scheduler = new RoundRobinScheduler();
+        var server = new ServerThreadWithScheduler(scheduler);
+
+        var commands = new TestCommand[5];
+        for (int i = 0; i < 5; i++)
+        {
+            commands[i] = new TestCommand(i + 1);
+            server.AddCommand(commands[i]);
+        }
+
+        server.Start();
+
+        Thread.Sleep(500);
+
+        var hardStop = new HardStopCommand(server);
+        server.AddCommand(hardStop);
+
+        server.Thread.Join(1000);
+
+        for (int i = 0; i < 5; i++)
+        {
+            Assert.True(commands[i].IsCompleted);
+            Assert.Equal(3, commands[i].Counter);
+        }
+    }
+}
